52db61d3eb52649f9eafe99a85ec04a1be4350d3
Christopher Faria <eldannyto@hotmail.com>
2023-10-04 10:10:04 -0300

    Upando restante aula 07 de SQL afetando DataScience

--- SQL/Aula 07/CriaçãoBancoBD.sql
+++ SQL/Aula 07/CriaçãoBancoBD.sql
@@ -67,8 +67,58 @@ FOREIGN KEY (id_cartao) REFERENCES cartao_debito(id),
 ADD CONSTRAINT fk_transacoes_cartao_credito
 FOREIGN KEY (id_cartao) REFERENCES cartao_credito(id);
 
+ALTER TABLE cliente
+MODIFY COLUMN telefone VARCHAR(15);
+
+INSERT INTO cliente (primeiro_nome, ultimo_nome, telefone, email)
+VALUES ('Christopher', 'Faria', '37991213397', 'eldannyto@hotmail.com');
+
+INSERT INTO conta_cliente (id_cliente, agencia)
+VALUES ('1', '4');
+
+INSERT INTO cartao_debito (id_conta_cliente, status)
+VALUES ('1', 'Ativo');
+
+INSERT INTO cartao_credito (id_conta_cliente, status)
+VALUES ('1', 'Ativo');
+
+/*INSERT INTO transacoes (id_cliente, id_conta_cliente, id_cartao, valor, tipo, descricao, info_pagamento)
+VALUES ('1', '1', '1', 82.35, 'Debito', 'PAG LOJA 1', 'LOJA 1 - BH/MG');*/
 
 
+/*
+DELIMITER //
+CREATE TRIGGER depois_de_inserir_transacao_credito
+AFTER INSERT ON transacoes FOR EACH ROW
+BEGIN
+    DECLARE valor_total DECIMAL(10,2);
 
+    SELECT SUM(valor) INTO valor_total FROM transacoes
+    WHERE id_conta_cliente = NEW.id_conta_cliente AND tipo = 'Credito';
 
+    UPDATE conta_cliente
+    SET fatura_credito = valor_total
+    WHERE id = NEW.id_conta_cliente;
+END;
+//
+DELIMITER ;
+*/
+
+/*
+DELIMITER //
+CREATE TRIGGER depois_de_inserir_transacao_debito
+AFTER INSERT ON transacoes FOR EACH ROW
+BEGIN
+    DECLARE valor_total DECIMAL(10,2);
+
+    SELECT SUM(valor) INTO valor_total FROM transacoes
+    WHERE id_conta_cliente = NEW.id_conta_cliente AND tipo = 'Debito';
+
+    UPDATE conta_cliente
+    SET saldo = -(valor_total)
+    WHERE id = NEW.id_conta_cliente;
+END;
+//
+DELIMITER ;
+*/
 
